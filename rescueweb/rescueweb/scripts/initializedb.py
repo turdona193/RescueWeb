a51e89b59e42af9014a2687dd94b4874b0421e9c
kosowsjm195 <kosowsjm195@potsdam.edu>
2013-04-18 17:05:41 -0400

    Fixed issue with a funny character in initializedb.py.

--- rescueweb/rescueweb/scripts/initializedb.py
+++ rescueweb/rescueweb/scripts/initializedb.py
@@ -52,16 +52,13 @@ Disclaimer: Campus Rescue Squad takes privacy very seriously. Any images shown o
         DBSession.add(model)
         
         model = Page(name = 'History', data = """The SUNY Potsdam Campus Rescue Squad started in 1992 as a group of college students that formed together to provide much needed medical coverage for SUNY Potsdam varsity athletic events as well as various other college events. </br>
-
-By 1993, CRS was providing coverage for most all athletic events when it was requested, many concerts at the Crane School of Music, as well as other various college events. At the end of the 1993-1994 school year, it was decided that the campus required 24/7 first response care, not just medical stand-bys. By September 2004, Campus Rescue was not only a student organization, but also a New York State DOH certified Emergency First Response Agency. Also, CRS was now able to sponsor members to take both EMT-Basic and EMT-D courses through SUNY Canton. NYS certified EMT’s were also on call each night for 8 hour tours of duty. </br>
-
 As responsibilities as a certified agency came, so did the necessity for communications. Through their menial budget CRS was able to purchase four radios. Along with those radios was a donation of 12 pagers, which finally allowed Campus Safety (now University Police) to activate CRS via pager alert. </br>
 
 Almost 17 years later, Campus Rescue continues the tradition of quality emergency medical care 24/7 while school is in session. Because of the dedication of true professional EMS providers, Campus Rescue continues to thrive as an important link in prehospital care in SUNY Potsdam. </br>
 
-The Squad today (as of 2008) has approximately 25 members, 14 of which are NYS certified EMT’s, 6 of which are currently enrolled in the NYS EMT-B curriculum, and 23 of which are American Heart Association BCLS certified in AED and CPR.</br>
+The Squad today (as of 2008) has approximately 25 members, 14 of which are NYS certified EMT's, 6 of which are currently enrolled in the NYS EMT-B curriculum, and 23 of which are American Heart Association BCLS certified in AED and CPR.</br>
 
-The Squad currently has 7 NYS DOH Part 800 compliant BLS jump bags. One bag is housed in University Police dispatch, one at the Crumb Library, and 2 are housed in the squad equipment room. CRS is approved through medical direction and NY to administer albuterol for respiratory emergencies and Epinephrine for anaphylaxis. The squad also owns an AED, housed in one of University Police’s patrol cars. AEDs are also housed in every building on campus due to the PAD program.</br>
+The Squad currently has 7 NYS DOH Part 800 compliant BLS jump bags. One bag is housed in University Police dispatch, one at the Crumb Library, and 2 are housed in the squad equipment room. CRS is approved through medical direction and NY to administer albuterol for respiratory emergencies and Epinephrine for anaphylaxis. The squad also owns an AED, housed in one of University Police's patrol cars. AEDs are also housed in every building on campus due to the PAD program.</br>
 
 Campus Rescue runs on average approximately 150 calls and standbys each year.""")
         DBSession.add(model)
