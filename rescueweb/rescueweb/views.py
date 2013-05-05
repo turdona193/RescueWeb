0c02f407dab0e99d21b9d5f91e97009780c3c30b
Tim Simmons <simmontj193@potsdam.edu>
2013-05-05 18:12:37 -0400

    Altered edit_standby. Now trying to pass the StandBy table from the databse to the webpage. Have changed 'add_edit_standby.py' and 'views.py'.

--- rescueweb/rescueweb/views.py
+++ rescueweb/rescueweb/views.py
@@ -657,9 +657,11 @@ def add_edit_certifications(request):
              permission='admin')
 def add_edit_standby(request):
     main = get_renderer('templates/template.pt').implementation()
+    all_standBy = DBSession.query(StandBy).order_by(StandBy.standbyid).all()
 
     return dict(title='Add/Edit Standby',
             main=main,
+            all_standBy = all_standBy,
             user=request.user
             )
 
