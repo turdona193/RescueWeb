fe0dbfa650a87d3892784bb7790a3972869511b2
Edward Banner <bannerem195@potsdam.edu>
2013-05-08 18:32:20 -0400

    Fixed calendars and commented out pyramid mailer stuff.
    Pyramid mailer stuff was causing everything to crash, so I commented every
    occurrence of it out. Run the following command:
    
        $ python setup.py develop
    
    This will (hopefully) get everything working again.

--- rescueweb/rescueweb/__init__.py
+++ rescueweb/rescueweb/__init__.py
@@ -29,7 +29,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings,
                           root_factory='rescueweb.models.RootFactory')
 
-    config.include('pyramid_mailer')
+    #config.include('pyramid_mailer')
 
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
