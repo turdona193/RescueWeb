4e7ffb0d0b0bae55c1bda70f7659d71208dc7fc8
Lee Page <pagel194@potsdam.edu>
2013-05-10 18:21:59 -0400

    got form to submit on pictures, but pictures object is empty?

--- rescueweb/rescueweb/__init__.py
+++ rescueweb/rescueweb/__init__.py
@@ -47,7 +47,6 @@ def main(global_config, **settings):
     config.add_route('events' , '/events')
     config.add_route('event' , '/event/{eventid}')
     config.add_route('pictures' , '/pictures')
-    config.add_route('category_pictures', '/category_pictures')
     config.add_route('join' , '/join')
     config.add_route('contact' , '/contact')
     config.add_route('links' , '/links')
